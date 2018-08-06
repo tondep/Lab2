0650bfdf95f3e4bd0f1c38e3057113b8281dfa83
tondep <692436@students.wits.ac.za>
2018-08-06 19:58:08 +0200

    Exercise 2.1

--- vector/size_capacity.cpp
+++ vector/size_capacity.cpp
@@ -1,22 +1,25 @@
-// size_capacity.cpp
-// Vector size versus capacity
-
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
-{
-    vector<int> vec;
-    cout << "vec: size: " << vec.size() 
-         << " capacity: " << vec.capacity() << endl;
+ {
+ vector<int> vec;
+ //The vec: size" is zero, capacity will be allocated space.
+ cout << "vec: size: " << vec.size()
+ << " capacity: " << vec.capacity() << endl;
 
-    for(int i = 0; i < 24; i++) {
-        vec.push_back(i);
-        cout << "vec: size: " << vec.size() 
-             << " capacity: " << vec.capacity() << endl;
-    }
+// The difference: vec.size()- give back the number of elements in a vector while,
+// vec.size()- gives the space allocated for the vector including that unoccupied by elements meant for expanding vector size. 
 
-    return 0;
-}
+ for(int i = 0; i < 24; i++) {
+ vec.push_back(i);
+ cout << "vec: size: " << vec.size()
+ << " capacity: " << vec.capacity() << endl;
+ }
+// The space allocation is done logically to balance between saving memory and reallocation,
+//when the vector size exceeds capacity is  the allocator will double the space thereby explaining 
+//0 1 2 4 8 16 32 capacity.
+ return 0;
+ }
