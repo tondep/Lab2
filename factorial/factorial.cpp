ddb1b629c815fa59df517b82995118347e5947fb
tondep <692436@students.wits.ac.za>
2018-08-07 02:31:01 +0200

    Exercise 4.1

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -4,9 +4,9 @@
 int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
 
 TEST_CASE("testing the factorial function") {
-    CHECK(factorial(0) == 1);
     CHECK(factorial(1) == 1);
     CHECK(factorial(2) == 2);
     CHECK(factorial(3) == 6);
     CHECK(factorial(10) == 3628800);
+    CHECK(factorial(11) == 39916800);
 }
